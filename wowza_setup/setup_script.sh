ac8b0c86fd5a496f7ce120b0bde18d4cf5366ade
Ian Zenoni <ian@wowza.com>
2025-08-04 13:55:02 -0600

    log if sdk exists for less confusion

--- wowza_setup/setup_script.sh
+++ wowza_setup/setup_script.sh
@@ -5,6 +5,12 @@ if [ -f "/wowza_setup/downloads/wse-plugin-caption-handlers-1.1.0.jar" ]; then
 else
 	echo "Getting wse-plugin-caption-handlers-1.1.0.jar"
 	wget -q -P /wowza_setup/downloads/ https://github.com/WowzaMediaSystems/wse-plugin-caption-handlers/releases/download/1.1.0/wse-plugin-caption-handlers-1.1.0.jar
+fi
+
+if [ -f "/wowza_setup/downloads/client-sdk-1.44.0.jar" ]; then
+	echo "File client-sdk-1.44.0.jar exists"
+else
 	echo "Getting client-sdk-1.44.0.jar"
 	wget -q -P /wowza_setup/downloads/ https://github.com/WowzaMediaSystems/wse-plugin-caption-handlers/releases/download/1.1.0/client-sdk-1.44.0.jar
 fi
+
