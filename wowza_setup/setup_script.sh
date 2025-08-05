5cba25b12a2f5a20851c4c3c31978e94c8c4f307
Ian Zenoni <ian@wowza.com>
2025-08-05 08:49:19 -0600

    remove older versions of jars if needed

--- wowza_setup/setup_script.sh
+++ wowza_setup/setup_script.sh
@@ -3,6 +3,7 @@
 if [ -f "/wowza_setup/downloads/wse-plugin-caption-handlers-1.1.0.jar" ]; then
 	echo "File wse-plugin-caption-handlers-1.1.0.jar exists"
 else
+	rm -f /wowza_setup/downloads/wse-plugin-caption-handlers*
 	echo "Getting wse-plugin-caption-handlers-1.1.0.jar"
 	wget -q -P /wowza_setup/downloads/ https://github.com/WowzaMediaSystems/wse-plugin-caption-handlers/releases/download/1.1.0/wse-plugin-caption-handlers-1.1.0.jar
 fi
@@ -10,6 +11,7 @@ fi
 if [ -f "/wowza_setup/downloads/client-sdk-1.44.0.jar" ]; then
 	echo "File client-sdk-1.44.0.jar exists"
 else
+	rm -f /wowza_setup/downloads/client-sdk-*
 	echo "Getting client-sdk-1.44.0.jar"
 	wget -q -P /wowza_setup/downloads/ https://github.com/WowzaMediaSystems/wse-plugin-caption-handlers/releases/download/1.1.0/client-sdk-1.44.0.jar
 fi
